10e4593b5a5af99036e8e29f2c72bd37611b2e49
曾翰林 <hl_zeng@droidhang.com>
2021-10-29 17:46:40 +0800

    add comment

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import Click from "./com/click"
 import "./style/index.scss"
 
 const m = new Render(document.body)
-const r = new Component()
-const c1 = new Component()
-const c2 = new Component()
-const c3 = new Component()
+const r = new Component() // 根组件
+const c1 = new Component() // 组件1
+const c2 = new Component() // 组件2
+const c3 = new Component() // 组件3
 c1.setText("Hello")
 c2.setText("World")
 c3.setText("!")
 c1.setAttr("class", "main1 text-2")
 c2.setAttr("class", "main2 text-2")
 c3.setAttr("class", "main3 text-2")
-const c4 = new Click()
+const c4 = new Click() // 自定义组件
 r.setText([c1, c2, c3, c4])
 m.mount(r)
